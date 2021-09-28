58e605c041d417a25ea6d8665b263ff8fed2e361
Elias Souza <89436479+Cavanha123@users.noreply.github.com>
2021-09-28 10:40:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+var datadoevento ="22"
+
+//data atual "21"
+
+if (datadoevento >21) {
+    console.log ("Cadastro Permitido")
+} else console.log ("Cadastro Negado"); ("Data Invalida")
+
+//CADASTRO CONCLUÍDO
+//INSERIR IDADE
+
+var idade =("18")
+
+if (idade >=18) {
+console.log ("Idade Permitida")
+} else console.log ("Idade não Permitida"); ("Cadastro Negado")
+
+//IDADE PERMITIDA
+//CONFERIR LISTA DE PARTICIPANTES 
+
+var participantes = "100"
+
+//limite de participantes "100"
+
+if (participantes <=100){
+    console.log ("Cadastro Concluído")
+} else console.log ("Cadastro Negado"); ("Limite de Participantes atingido")
+
+//CADASTRO CONCLUÍDO (AGUARDAR DIA DO EVENTO)
